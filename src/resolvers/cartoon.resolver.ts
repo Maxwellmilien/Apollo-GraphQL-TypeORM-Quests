e1cb94b37a43ff056c069619ec9918d868be8de2
Maximilien <maximilien.philippe@protonmail.com>
2025-03-24 23:14:00 +0100

    add createCartoon to cartoon resolver

--- src/resolvers/cartoon.resolver.ts
+++ src/resolvers/cartoon.resolver.ts
@@ -1,24 +1,34 @@
-import { Console } from "console";
 import { default as cartoons } from "../../datas/dataset.json";
 import { Cartoon } from "../types/cartoon.type";
+
+const datas = [...cartoons];
 
 type GetOneCartoonByIdArgs = {
   id: string;
 };
 
 export function getCartoons(_: unknown, _args: unknown): Cartoon[] {
-  return cartoons as Cartoon[];
+  return datas as Cartoon[];
 }
 
 export function getOneCartoonById(
   _: unknown,
   args: GetOneCartoonByIdArgs,
 ): Cartoon {
-  return cartoons.find((cartoon) => cartoon.id === +args.id) as Cartoon;
+  return datas.find((cartoon) => cartoon.id === +args.id) as Cartoon;
 }
 
-export function createCartoon(_: unknown, args: { cartoon: Cartoon }): string {
-  const cartoon: Cartoon = args;
-  console.log(cartoon);
-  return "1234";
+export function createCartoon(
+  _: unknown,
+  args: { cartoon: Omit<Cartoon, "id"> },
+): string {
+  const id_cartoon = Date.now();
+  const cartoon: Cartoon = { id: id_cartoon, ...args.cartoon };
+
+  cartoon.personnages.forEach((c, i) => {
+    c.id = i;
+  });
+
+  datas.push(cartoon);
+  return "" + cartoon.id;
 }
