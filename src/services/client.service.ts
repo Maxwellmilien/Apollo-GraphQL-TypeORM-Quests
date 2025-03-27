a68e330a35296cbb56e7edc98ef5913f71faa042
Maximilien <maximilien.philippe@protonmail.com>
2025-03-27 06:30:50 +0100

    add cartoon.entity

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm";
+import { Cartoon } from "../entitys/cartoon.entity";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [],
+  entities: [Cartoon],
   synchronize: true, //pas en prod
 });
